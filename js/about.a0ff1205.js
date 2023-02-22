5d4bb0068dd3605f1cc1d25f0fe4169b7df589e9
An TaeHyeon <47134267+mysourcefile@users.noreply.github.com>
2023-02-22 12:28:25 +0900

    Add files via upload

---
+++ js/about.a0ff1205.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_project"]=self["webpackChunkvue_project"]||[]).push([[443],{381:function(e,n,u){u.r(n),u.d(n,{default:function(){return l}});var t=u(252);const r={class:"about"},a=(0,t._)("h1",null,"This is an about page",-1),c=[a];function s(e,n){return(0,t.wg)(),(0,t.iD)("div",r,c)}var o=u(744);const i={},f=(0,o.Z)(i,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.a0ff1205.js.map
